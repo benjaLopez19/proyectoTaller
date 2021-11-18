3b4356f5517c33472dcf48673b01a2a7bb1da2ce
benjaLopez19 <82004309+benjaLopez19@users.noreply.github.com>
2021-11-18 16:51:58 -0300

    cambiosBusqueda

--- proyectoTaller/settings.py
+++ proyectoTaller/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'proyectotaller',
         'USER': 'postgres',
-        'PASSWORD': 'papipa',
+        'PASSWORD': '1234',
         'HOST':'127.0.0.1',
         'DATABASE_PORT':'5432'
     }
