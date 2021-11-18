d7a61c9a55897a0fbc36195f82c3cb2938732cef
GeorgeV431 <45008156+GeorgeV431@users.noreply.github.com>
2021-11-18 18:35:30 -0300

    fix home

--- proyectoTaller/settings.py
+++ proyectoTaller/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'proyectotaller',
         'USER': 'postgres',
-        'PASSWORD': 'fn2187',
+        'PASSWORD': 'papipa',
         'HOST':'127.0.0.1',
-        'PORT':'5433',
+        'PORT':'5432',
     }
 
 }
