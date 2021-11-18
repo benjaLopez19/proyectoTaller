ccf7930188499a797d0a4be30234a3a9d3058713
AaronIguana <75752784+AaronIguana@users.noreply.github.com>
2021-11-18 17:24:52 -0300

    xd

--- proyectoTaller/settings.py
+++ proyectoTaller/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'proyectotaller',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'dorothea019',
         'HOST':'127.0.0.1',
         'DATABASE_PORT':'5432'
     }
