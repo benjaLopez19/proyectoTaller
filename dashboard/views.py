b626cdada2204dbf09b2f85160182f7ca699a5b7
GeorgeV431 <45008156+GeorgeV431@users.noreply.github.com>
2021-11-21 04:08:05 -0300

    more graficos

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,7 @@ from dashboard.forms import FormularioBusqueda
 from dashboard.models import Data
 from django.utils.html import strip_tags
 import json
+from datetime import datetime
 
 
 def home(request):
@@ -69,7 +70,7 @@ def graficos(request):
 
     i=0
     k=0
-    while i < 200:
+    while i < 150:
         if busqueda[i].intent==None :
             next
 
@@ -79,6 +80,9 @@ def graficos(request):
 
         res = next((sub for sub in intents if sub['intent'] == busqueda[i].intent), None)
         if res == None:
+            if busqueda[i].intent==None:
+                i+=1
+                next
             diccionario ={"intent":busqueda[i].intent,"cantidad":1}
             intents.append(diccionario)
         else   :
@@ -91,21 +95,62 @@ def graficos(request):
 
     print("------------ LISTA -----------")
     while k<len(intents):
-        print(intents)
         palabras.append(intents[k].get("intent"))
         numeros.append(intents[k].get("cantidad"))
+        print(intents[k])
         k+=1
+        
 
     print("largo",len(intents))
-
-
-    #=========================================================================================FECHA
-    
-
     js_data1 = json.dumps(palabras)
     js_data2 = json.dumps(numeros)
-    return render(request, "graficos.html", {"data1": js_data1,"data2": js_data2})
+    #=========================================================================================FECHA
+    fechas =[]
+    palabrasF=[]
+    numerosF=[]
 
+    i=0
+    k=0
+    while i < 150:
+        
+        if busqueda[i].fecha==None :
+            next
+
+        if i==0 : 
+            date=busqueda[i].fecha.strftime("%d-%m-%Y")
+            diccionario ={"fecha":date,"cantidad":1}
+            fechas.append(diccionario) #agregar primer elemento al arreglo
+
+        res = next((sub for sub in fechas if sub['fecha'] == busqueda[i].fecha.strftime("%d-%m-%Y")), None)
+        if res == None:
+            diccionario ={"fecha":busqueda[i].fecha.strftime("%d-%m-%Y"),"cantidad":1}
+            fechas.append(diccionario)
+        else   :
+            index = next((index for (index, d) in enumerate(fechas) if d["fecha"] == busqueda[i].fecha.strftime("%d-%m-%Y")), None)
+            num=res.get("cantidad")
+            num+=1
+            fechas[index].update({"cantidad":num})
+
+        i+=1
+    
+
+    print("------------ LISTA -----------")
+    while k<len(fechas):
+        palabrasF.append(fechas[k].get("fecha"))
+        numerosF.append(fechas[k].get("cantidad"))
+        print(fechas[k])
+        print(palabrasF[k])
+        k+=1
+
+    print("largo",len(fechas))
+    js_dataF1 = json.dumps(palabrasF)
+    js_dataF2 = json.dumps(numerosF)
+
+    return render(request, "graficos.html", {"data1": js_data1,"data2": js_data2,"dataF1": js_dataF1,"dataF2": js_dataF2})
+
+#=========================================================================================
+#==============================BUSQUEDA===================================================
+#=========================================================================================
 
 def busqueda(request):
 
